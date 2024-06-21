fcbf8f1beedd4817c2c4dea098f22d570b9343cd
waleedo0o7 <54623281+waleedo0o7@users.noreply.github.com>
2024-06-21 15:54:56 +0300

    fix img height with new calsses

--- blog-list.php
+++ blog-list.php
@@ -19,16 +19,13 @@
             <div class="col-lg-8">
 
 
-                <div class="co-related-blogs mb-5">
-
-
+                <div class="co-blogs-list mb-5">
 
                     <div class="row">
 
-
-                        <?php foreach (range(0, 11) as $i) { ?>
+                        <?php foreach (range(0, 1) as $i) { ?>
                             <div class="col-sm-6">
-                                <div class="one-news-style-2 mb-2">
+                                <div class="one-news-style-2 blog mb-2">
                                     <a href="blog-single.php?dir=rtl"> <img src="assets/images/blog.jpg" class="img-fluid image"> </a>
                                     <a href="blog-single.php?dir=rtl" class="title bold mb-1 color-black"> 10 عبادات مهمة في فصل الشتاء عبادات مهمة في فصل الشتاء عبادات مهمة في فصل الشتاء عبادات مهمة في فصل الشتاءعبادات مهمة في فصل الشتاء عبادات مهمة في فصل الشتاء </a>
                                     <div class="d-flex">
@@ -52,7 +49,109 @@
                             </div>
                         <?php } ?>
 
+                        <?php foreach (range(0, 1) as $i) { ?>
+                            <div class="col-sm-6">
+                                <div class="one-news-style-2 file mb-2">
+                                    <a href="blog-single.php?dir=rtl"> <img src="assets/images/file.jpg" class="img-fluid image"> </a>
+                                    <a href="blog-single.php?dir=rtl" class="title bold mb-1 color-black"> 10 عبادات مهمة في فصل الشتاء عبادات مهمة في فصل الشتاء عبادات مهمة في فصل الشتاء عبادات مهمة في فصل الشتاءعبادات مهمة في فصل الشتاء عبادات مهمة في فصل الشتاء </a>
+                                    <div class="d-flex">
+                                        <p class="mb-2 date small color-gray me-2"> <i class="fa fa-clock"></i> فبراير 11, 2024 </p>
+                                        <p class="mb-2 views small color-gray"> <i class="fa fa-eye"></i> 150 </p>
+                                    </div>
 
+
+
+                                    <ul class="reset-list social-media-icons-colored icons-sm icons-start mb-4">
+                                        <li><a href="#" class="color-black facebook"> <i class="fab fa-facebook-f"></i> </a></li>
+                                        <li><a href="#" class="color-black twitter"> <i class="fab fa-x-twitter"></i> </a></li>
+                                        <li><a href="#" class="color-black youtube"> <i class="fab fa-youtube"></i> </a></li>
+                                        <li><a href="#" class="color-black instagram"> <i class="fab fa-instagram"></i> </a></li>
+                                        <li><a href="#" class="color-black telegram"> <i class="fab fa-telegram"></i> </a></li>
+                                        <li><a href="#" class="color-black whatsapp"> <i class="fab fa-whatsapp"></i> </a></li>
+                                    </ul>
+
+
+                                </div>
+                            </div>
+                        <?php } ?>
+
+                        <?php foreach (range(0, 1) as $i) { ?>
+                            <div class="col-sm-6">
+                                <div class="one-news-style-2 edition mb-2">
+                                    <a href="blog-single.php?dir=rtl"> <img src="assets/images/editions.jpg" class="img-fluid image"> </a>
+                                    <a href="blog-single.php?dir=rtl" class="title bold mb-1 color-black"> 10 عبادات مهمة في فصل الشتاء عبادات مهمة في فصل الشتاء عبادات مهمة في فصل الشتاء عبادات مهمة في فصل الشتاءعبادات مهمة في فصل الشتاء عبادات مهمة في فصل الشتاء </a>
+                                    <div class="d-flex">
+                                        <p class="mb-2 date small color-gray me-2"> <i class="fa fa-clock"></i> فبراير 11, 2024 </p>
+                                        <p class="mb-2 views small color-gray"> <i class="fa fa-eye"></i> 150 </p>
+                                    </div>
+
+
+
+                                    <ul class="reset-list social-media-icons-colored icons-sm icons-start mb-4">
+                                        <li><a href="#" class="color-black facebook"> <i class="fab fa-facebook-f"></i> </a></li>
+                                        <li><a href="#" class="color-black twitter"> <i class="fab fa-x-twitter"></i> </a></li>
+                                        <li><a href="#" class="color-black youtube"> <i class="fab fa-youtube"></i> </a></li>
+                                        <li><a href="#" class="color-black instagram"> <i class="fab fa-instagram"></i> </a></li>
+                                        <li><a href="#" class="color-black telegram"> <i class="fab fa-telegram"></i> </a></li>
+                                        <li><a href="#" class="color-black whatsapp"> <i class="fab fa-whatsapp"></i> </a></li>
+                                    </ul>
+
+
+                                </div>
+                            </div>
+                        <?php } ?>
+
+                        <?php foreach (range(0, 1) as $i) { ?>
+                            <div class="col-sm-6">
+                                <div class="one-news-style-2 quote mb-2">
+                                    <a href="blog-single.php?dir=rtl"> <img src="assets/images/quotes.jpg" class="img-fluid image"> </a>
+                                    <a href="blog-single.php?dir=rtl" class="title bold mb-1 color-black"> 10 عبادات مهمة في فصل الشتاء عبادات مهمة في فصل الشتاء عبادات مهمة في فصل الشتاء عبادات مهمة في فصل الشتاءعبادات مهمة في فصل الشتاء عبادات مهمة في فصل الشتاء </a>
+                                    <div class="d-flex">
+                                        <p class="mb-2 date small color-gray me-2"> <i class="fa fa-clock"></i> فبراير 11, 2024 </p>
+                                        <p class="mb-2 views small color-gray"> <i class="fa fa-eye"></i> 150 </p>
+                                    </div>
+
+
+
+                                    <ul class="reset-list social-media-icons-colored icons-sm icons-start mb-4">
+                                        <li><a href="#" class="color-black facebook"> <i class="fab fa-facebook-f"></i> </a></li>
+                                        <li><a href="#" class="color-black twitter"> <i class="fab fa-x-twitter"></i> </a></li>
+                                        <li><a href="#" class="color-black youtube"> <i class="fab fa-youtube"></i> </a></li>
+                                        <li><a href="#" class="color-black instagram"> <i class="fab fa-instagram"></i> </a></li>
+                                        <li><a href="#" class="color-black telegram"> <i class="fab fa-telegram"></i> </a></li>
+                                        <li><a href="#" class="color-black whatsapp"> <i class="fab fa-whatsapp"></i> </a></li>
+                                    </ul>
+
+
+                                </div>
+                            </div>
+                        <?php } ?>
+
+                        <?php foreach (range(0, 1) as $i) { ?>
+                            <div class="col-sm-6">
+                                <div class="one-news-style-2 caricature mb-2">
+                                    <a href="blog-single.php?dir=rtl"> <img src="assets/images/caricature-<?php echo $i +1 ?>.jpg" class="img-fluid image"> </a>
+                                    <a href="blog-single.php?dir=rtl" class="title bold mb-1 color-black"> 10 عبادات مهمة في فصل الشتاء عبادات مهمة في فصل الشتاء عبادات مهمة في فصل الشتاء عبادات مهمة في فصل الشتاءعبادات مهمة في فصل الشتاء عبادات مهمة في فصل الشتاء </a>
+                                    <div class="d-flex">
+                                        <p class="mb-2 date small color-gray me-2"> <i class="fa fa-clock"></i> فبراير 11, 2024 </p>
+                                        <p class="mb-2 views small color-gray"> <i class="fa fa-eye"></i> 150 </p>
+                                    </div>
+
+
+
+                                    <ul class="reset-list social-media-icons-colored icons-sm icons-start mb-4">
+                                        <li><a href="#" class="color-black facebook"> <i class="fab fa-facebook-f"></i> </a></li>
+                                        <li><a href="#" class="color-black twitter"> <i class="fab fa-x-twitter"></i> </a></li>
+                                        <li><a href="#" class="color-black youtube"> <i class="fab fa-youtube"></i> </a></li>
+                                        <li><a href="#" class="color-black instagram"> <i class="fab fa-instagram"></i> </a></li>
+                                        <li><a href="#" class="color-black telegram"> <i class="fab fa-telegram"></i> </a></li>
+                                        <li><a href="#" class="color-black whatsapp"> <i class="fab fa-whatsapp"></i> </a></li>
+                                    </ul>
+
+
+                                </div>
+                            </div>
+                        <?php } ?>
 
                     </div>
 
