80e95ec8b3be0f4ce0937c59a30b84543eeccfa9
waleedo0o7 <54623281+waleedo0o7@users.noreply.github.com>
2025-02-10 15:12:14 +0200

    finsih 404 page

---
+++ error-404.php
@@ -0,0 +1,38 @@
+<?php include 'layout-header.php'; ?>
+
+
+
+
+<main class="main pb-5">
+
+    <div class="container">
+
+
+
+        <div class="error text-center">
+
+            <div style="font-size: 270px; line-height: 1; font-weight: bold; color : #273649">
+                404
+            </div>
+
+            <!-- <img src="assets/images/error-404.png" class="img-fluid" style="width: 600px;"> -->
+
+            <h3 class="mb-4"> يبدو ان هناك خطاء في الرابط <br />ويمكنك البحث عن الموضوع من هنا </h3>
+
+            <div class="col-md-6 m-auto">
+
+                <div class="form-group co-input-with-btn mb-0">
+                    <input type="text" class="form-control search" placeholder="بحث">
+                    <button class="btn btn-primary button"> <i class="fa fa-search"></i></button>
+                </div>
+
+            </div>
+
+
+        </div>
+
+    </div>
+
+</main> <!-- main -->
+
+<?php include 'layout-footer.php'; ?>
